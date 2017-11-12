8a3f52f6af0058e176818dc619a8c8a9b0fbfab8
googlr <xg626@gmail.com>
2017-11-12 15:35:41 -0500

    Extract the corresponding data from the array

--- d3ReadData.js
+++ d3ReadData.js
@@ -15,8 +15,27 @@ d3.csv("http://0.0.0.0:8000/car.csv", function(error, data){
 					"origin"];
 	
 	//
-	dataLength = data.length;
-	console.log(dataLength);
+	dataRow = data.length;
+	console.log(dataRow);
+
+	//select value from the page, default
+	var sel_x = $("#sel-x").val(), 
+		sel_y = $("#sel-y").val();
+	var mpg_min = $("#mpg-min").val(), 
+		mpg_max = $("#mpg-max").val();
+
+	// Extract the corresponding data from the array
+	var xData = [],
+		yData = [];
+	for( var i=0; i<dataRow; i++){
+		xData.push( parseInt( data[i][ sel_x ] ) );
+		yData.push( parseInt( data[i][ sel_y ] ) );
+	}
+
+	console.log(xData);
+	console.log(yData);
+	console.log(mpg_min);
+	console.log(mpg_max);
     //format data if required...
     //draw chart
     //var parsed = d3.csvParse(data);
