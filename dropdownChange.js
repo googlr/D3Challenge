1fcbd24d00c87ffebc350168544d9d0180d89906
WLSK <cw2661@nyu.edu>
2017-11-16 23:37:19 -0500

    fix!!!!

--- dropdownChange.js
+++ dropdownChange.js
@@ -1,60 +1,14 @@
-// Read the data
-d3.csv("http://0.0.0.0:8000/car.csv", function(error, data){
-  		if (error) throw error;
-		console.log(data[0]);
-
-		// Check the data
-		var properties = ["name",
-					"mpg",
-					"cylinders",
-					"displacement",
-					"horsepower",
-					"weight",
-					"acceleration",
-					"model.year",
-					"origin"];
-		//
-dataRow = data.length;
-//console.log(dataRow);
-
-//select value from the page, default
-/*
-var sel_x = $("#sel-x").val(), 
-	sel_y = $("#sel-y").val();
-var mpg_min = $("#mpg-min").val(), 
-	mpg_max = $("#mpg-max").val();
-alert("sel-x: " + sel_x 
-	+ " sel-y: " + sel_y 
-	+ "   mpg_min: " + mpg_min 
-	+ " mpg_max: " + mpg_max );
-*/
-// Extract the corresponding data from the array
-/*
-var xData = [],
-	yData = [];
-for( var i=0; i<dataRow; i++){
-	xData.push( parseInt( data[i][ sel_x ] ) );
-	yData.push( parseInt( data[i][ sel_y ] ) );
-}
-console.log(xData);
-*/
-//console.log(yData);
-//console.log(mpg_min);
-//console.log(mpg_max);
-  
-
-});
 
 // Create Event Handlers for mouse
-      function handleMouseOver(d) {  // Add interactivity
+function handleMouseOver(d) { // Add interactivity
 
-            // Use D3 to select element, change color and size
-            $("#hovered").html( d["name"] );
-          }
+	// Use D3 to select element, change color and size
+	$("#hovered").html(d["name"]);
+}
 
-      function handleMouseOut(d, i) {
-            // Use D3 to select element, change color back to normal
-          }
+function handleMouseOut(d, i) {
+	// Use D3 to select element, change color back to normal
+}
 
 var draw = function(xdata, ydata, low, high) {
 	d3.selectAll("svg > *").remove();
@@ -92,12 +46,11 @@ var draw = function(xdata, ydata, low, high) {
 			return d["mpg"] >= low && d["mpg"] <= high;
 		});
 		// making scale	
-		//console.log(data.length);
 		var scaleX = d3.scaleLinear()
 			.range([0, width])
 			.domain([d3.min(data, function(d) {
 				return d[xdata];
-			}) / 1.5, d3.max(data, function(d) {
+			}) / 1.05, d3.max(data, function(d) {
 				return d[xdata];
 			})]);
 
@@ -105,10 +58,10 @@ var draw = function(xdata, ydata, low, high) {
 			.range([height, 0])
 			.domain([d3.min(data, function(d) {
 				return d[ydata];
-			}) / 1.5, d3.max(data, function(d) {
+			}) / 1.05, d3.max(data, function(d) {
 				return d[ydata];
 			})]);
-
+			
 		// Add the X Axis
 		svg.append("g")
 			.attr("class", "axis")
@@ -120,7 +73,7 @@ var draw = function(xdata, ydata, low, high) {
 		svg.append("text")
 			.attr("transform",
 				"translate(" + (width) + " ," +
-				(height + margin.top) + ")")
+				(height + margin.top- 10) + ")")
 			.style("text-anchor", "middle")
 			.text(xdata);
 
@@ -133,7 +86,7 @@ var draw = function(xdata, ydata, low, high) {
 		svg.append("text")
 			.attr("transform", "rotate(-90)")
 			.attr("y", 0 - margin.left)
-			.attr("x", 0 )
+			.attr("x", 0)
 			.attr("dy", "1em")
 			.style("text-anchor", "middle")
 			.text(ydata);
@@ -154,7 +107,7 @@ var draw = function(xdata, ydata, low, high) {
 			})
 			.attr("r", 2)
 			.on("mouseover", handleMouseOver)
-          	.on("mouseout", handleMouseOut);
+			.on("mouseout", handleMouseOut);
 
 		// add text
 
@@ -162,47 +115,36 @@ var draw = function(xdata, ydata, low, high) {
 
 };
 
-
-action = function(){
-	// Call Draw
-        var sel_x = $("#sel-x").val(), 
-			sel_y = $("#sel-y").val();
-		var mpg_min = $("#mpg-min").val(), 
-			mpg_max = $("#mpg-max").val();
-		alert("sel-x: " + sel_x 
-			+ " sel-y: " + sel_y 
-			+ "   mpg_min: " + mpg_min 
-			+ " mpg_max: " + mpg_max );
-		draw( sel_x, sel_y, mpg_min, mpg_max );
+action = function() {
+	var sel_x = $("#sel-x").val(),
+		sel_y = $("#sel-y").val();
+	var mpg_min = $("#mpg-min").val(),
+		mpg_max = $("#mpg-max").val();
+	draw(sel_x, sel_y, mpg_min, mpg_max);
 }
 
 // Draw the chart
 $(document).ready(function() {
-	//draw("displacement", "mpg", 0, 30);
-	//draw( sel_x, sel_y, mpg_min, mpg_max );
+
 	$('#update').click(function() {
-		//draw("displacement", "weight", 0, 30);
 		action();
-		//draw( sel_x, sel_y, mpg_min, mpg_max );
 	});
 
 	$("#sel-x").change(function() {
-        //alert( $('option:selected', this).text() );
-        // Call Draw
-        action();
-    });
+		action();
+	});
 
-    $("#sel-y").change(function() {
-        //alert( $('option:selected', this).text() );
-        action();
-    });
+	$("#sel-y").change(function() {
+		action();
+	});
 });
 
 // Hover
-$(document).ready(function(){
-    $("hovered").hover(function(){
-        $(this).css("background-color", "yellow");
-        }, function(){
-        $(this).css("background-color", "pink");
-    });
+$(document).ready(function() {
+	action();
+	$("hovered").hover(function() {
+		$(this).css("background-color", "yellow");
+	}, function() {
+		$(this).css("background-color", "pink");
+	});
 });
